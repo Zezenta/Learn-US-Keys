ee230aa8aecf2076f358368034022f6d0a39a0ac
José Miguel <urux11@gmail.com>
2025-09-23 06:02:35 -0500

    Use react-scan to know how terribly slow this is

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,15 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 
+if (process.env.NODE_ENV === "development") {
+    import("react-scan").then(({ scan }) => {
+        scan({
+        log: true,   // ver renders en la consola
+        paint: true, // resaltar visualmente los renders
+        });
+    });
+}
+
 createRoot(document.getElementById("root")).render(
     <StrictMode>
         <App />
